ac0db3e27bd7bb110110317620197a4e8d3337da
Miuler Blas <miulerbm00@gmail.com>
2023-09-26 11:00:23 -0500

    Mejora en presentacion

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 import streamlit as st
 
+# Establece el estilo de la barra lateral
+st.markdown(
+    """
+<style>
+.sidebar .sidebar-content {
+    background-image: linear-gradient(#2e7bcf,#2e7bcf);
+    color: white;
+}
+</style>
+""",
+    unsafe_allow_html=True,
+)
+
 from predict_page import show_predict_page
 from explore_page import show_explore_page
 
 options = ("Predecir", "Explorar")
 page = st.sidebar.selectbox("Elija una opción", options)
 
-if page =="Predecir":
+if page == "Predecir":
     show_predict_page()
 else:
     show_explore_page()
--- predict_page.py
+++ predict_page.py
@@ -1,7 +1,6 @@
 import streamlit as st
-import pickle # To load the data
+import pickle  # To load the data
 import numpy as np
-
 
 # Se crea la función para cargar el modelo entrenado
 def load_model():
@@ -10,60 +9,54 @@ def load_model():
     return data
 
 data = load_model()
-
-# Accedemos a los diferentes elementos del archivo
 regressor = data["model"]
 le_country = data["le_country"]
 le_education = data["le_education"]
 
+
 def show_predict_page():
-    st.title("Herramienta de Predicción de Salario (Software Development)")
+    st.title("Calculadora de Salario (Desarrollo de Software)")
 
-    st.write("""### Introduzca sus datos""")
+    # Añadir un contenedor para centrar el formulario
+    form = st.form(key='prediction_form')
+    col1, col2 = form.columns(2)
 
-    countries = (
-        "United States of America",
-        "Germany",
-        "United Kingdom of Great Britain and Northern Ireland",
-        "Canada",
-        "India",
-        "France",
-        "Netherlands",
-        "Australia",
-        "Brazil",
-        "Spain",
-        "Sweden",
-        "Italy",
-        "Poland",
-        "Switzerland",
-        "Denmark",
-        "Norway",
-        "Israel",
-    )
+    with col1:
+        countries = (
+            "United States of America",
+            "Germany",
+            "United Kingdom of Great Britain and Northern Ireland",
+            "Canada",
+            "India",
+            "France",
+            "Netherlands",
+            "Australia",
+            "Brazil",
+            "Spain",
+            "Sweden",
+            "Italy",
+            "Poland",
+            "Switzerland",
+            "Denmark",
+            "Norway",
+            "Israel",
+        )
 
+        country = st.selectbox("País", countries)
 
+    with col2:
+        education = (
+            "Less than a Bachelors",
+            "Bachelor’s degree",
+            "Master’s degree",
+            "Post grad",
+        )
 
-    education = (
-        "Less than a Bachelors",
-        "Bachelor’s degree",
-        "Master’s degree",
-        "Post grad",
-    )
+        education = st.selectbox("Educación", education)
 
-    # Las tuplas definidas arriba se empleand como las opciones del
-    # selectbox. Tanto para country como para education
-    # El valor seleccionado en la interfaz será cargado a la variable definida:
-    country = st.selectbox("País", countries)
-    education = st.selectbox("Educación", education)
-
-    # Ahora, para los años de experiencia:
     experience = st.slider("Años de Experiencia", 0, 50, 3)
-    # Límite inferior, lím superior y valor default
+    ok = form.form_submit_button("Calcular Salario")
 
-    # Ahora, para el botón de Calcular Salario:
-    ok = st.button("Predecir mi salario")
-
-    # Si es que damos click a "ok", será true:
     if ok:
         X = np.array([[country, education, experience]])
         X[:, 0] = le_country.transform(X[:, 0])
@@ -71,4 +64,4 @@ def show_predict_page():
         X = X.astype(float)
 
         salary = regressor.predict(X)
-        st.subheader(f"El salario será aproximadamente: ${salary[0]:,.02f}")
+        st.subheader(f"El salario estimado es: ${salary[0]:,.02f}")
